9117852cef73fba0af811ef1e617e45ac93171cf
Jay <shethju@gmail.com>
2020-11-27 02:35:44 -0800

    Create TwoSum.java

---
+++ TwoSum.java
@@ -0,0 +1,25 @@
+package lc75;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+// https://leetcode.com/problems/two-sum/
+public class TwoSum {
+  public static void main(String[] args) {
+    int[] nums = {2,7,11,15};
+    int target = 9;
+    System.out.println(twoSum(nums, target));
+  }
+
+  public static int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    for (int i = 0; i < nums.length; i++) {
+      int compliment = target - nums[i];
+      if (map.containsKey(compliment)) {
+        return new int[] {map.get(compliment), i};
+      }
+      map.put(nums[i], i);
+    }
+    throw new IllegalArgumentException("No real combinations");
+  }
+}
